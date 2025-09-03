9a4775ba6c2b2237bf8346e7ddabddb6135b2f04
Kitharbbbb <kittipol20245@gmail.com>
2025-09-04 00:18:25 +0700

    jwt file success

---
+++ gormMiddleware/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+const (
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
+)
+
+func authRequired(c *fiber.Ctx) error {
+	// Get JWT from cookies
+	cookie := c.Cookies("jwt")
+	if cookie == "" {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	//func ParseWithClaims(tokenString string, claims Claims, keyFunc Keyfunc, options ...ParserOption) (*Token, error)
+	// tokenString ข้อความ JWT ที่เราได้มา
+	// claims struct ที่เราคาดหวังจะใส่ข้อมูล JWT ลงไป (ที่นี่คือ &jwt.StandardClaims{})
+	// keyFunc ฟังก์ชันที่บอกว่าจะใช้ key อะไรในการ verify signature ของ JWT
+	// options (ไม่บังคับ) ใส่ parser option เพิ่มเติม เช่น validation พิเศษ
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tSecretKey), nil
+	})
+
+	//ตรวจสอบความถูกต้องของ token
+	if err != nil || !token.Valid {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	return c.Next() //ไป handler ถัดไป
+}
+
+func main() {
+	// Configure your PostgreSQL database details here
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// New logger for detailed SQL logging
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	db.AutoMigrate(&Book{})
+
+	app := fiber.New()
+
+	// add For prevent auth
+	app.Use("/books", authRequired)
+
+	// CRUD routes = API Set book ตัวเดิม
+
+	app.Post("/register", func(c *fiber.Ctx) error {
+		return createUser(db, c)
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		return loginUser(db, c)
+	})
+
+	app.Post("/logout", authRequired, func(c *fiber.Ctx) error {
+		return logoutUser(c)
+	})
+
+	app.Get("/books", func(c *fiber.Ctx) error {
+		return c.JSON(GetBooks(db))
+	})
+	app.Get("/books/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		book := GetBook(db, id)
+		return c.JSON(book)
+	})
+	app.Post("/books", func(c *fiber.Ctx) error {
+		var book Book
+		if err := c.BodyParser(&book); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		CreateBook(db, &book)
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		return c.JSON(fiber.Map{
+			"message": "Book created successfully",
+			"book":    book,
+		})
+	})
+	app.Put("/books/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		book := GetBook(db, id)
+		if err := c.BodyParser(&book); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		book.ID = uint(id)
+		if err := UpdateBook(db, book); err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		return c.JSON(fiber.Map{
+			"message": "Book updated successfully",
+			"book":    book,
+		})
+	})
+	app.Delete("/books/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
+		}
+		DeleteBook(db, uint(id))
+		return c.JSON(fiber.Map{
+			"message": "Book deleted successfully",
+		})
+	})
+
+	app.Listen(":3000")
+}
